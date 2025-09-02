e1448f11698018f9bb7084e645aeb6f86a2e077b
Justin B. Ramas <142135986+senRyuu286@users.noreply.github.com>
2025-09-02 22:03:52 +0800

    Improve mobile navigation responsiveness
    Updated script.js and style.css to enhance the mobile navigation menu's responsiveness. The menu now adjusts its padding, width, and flex direction based on viewport width, providing a better user experience on tablets and smaller devices. Also refactored CSS selectors for consistency.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event){
     var navmobilejs = document.getElementsByClassName("nav-burger");
     var navmobilejscontent = document.getElementsByClassName("nav-mobile-nav");
-    var headernav = document.getElementsByClassName("header-nav");
+    var medwidthtablet = window.matchMedia("(max-width: 770px)");
+    var medwidthcp = window.matchMedia("(max-width: 670px)");
+
+
     
     navmobilejs[0].addEventListener("click", function() {
         if (navmobilejscontent[0].style.maxHeight) {
             navmobilejscontent[0].style.maxHeight = null;
             navmobilejscontent[0].style.padding = "0px";
         } else {
-            navmobilejscontent[0].style.maxHeight = navmobilejscontent[0].scrollHeight + "px";
-            navmobilejscontent[0].style.padding = "10px 15px 10px 15px";
+             if (medwidthcp.matches) {
+                navmobilejscontent[0].style.padding = "0px";
+                navmobilejscontent[0].style.maxHeight = navmobilejscontent[0].scrollHeight + "px";
+                navmobilejscontent[0].style.flexDirection = "column";
+                navmobilejscontent[0].style.width = "30vw";
+            } else if (medwidthtablet.matches) {
+                navmobilejscontent[0].style.maxHeight = navmobilejscontent[0].scrollHeight + "px";
+                navmobilejscontent[0].style.padding = "20px 30px 20px 30px";
+                navmobilejscontent[0].style.flexDirection = "row";
+                navmobilejscontent[0].style.width = "50vw";
+            }
         }
     });
-});
+});                     
